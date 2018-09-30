3097f0903d4d779930eaa52196290006cf61e6df
k2works <kakimomokuri@gmail.com>
2018-09-30 12:11:38 +0900

    開発 条件を修正

--- sam-app/fizz_buzz/src/fizz_buzz.js
+++ sam-app/fizz_buzz/src/fizz_buzz.js
@@ -1,12 +1,12 @@
 module.exports = class FizzBuzz {
   static generate(number) {
     let value = number;
-    if (number % 3 === 0) {
+    if (number % 3 === 0 && number % 5 === 0) {
+      value = 'FizzBuzz';
+    } else if (number % 3 === 0) {
       value = 'Fizz';
     } else if (number % 5 === 0) {
       value = 'Buzz';
-    } else if (number % 3 === 0 && number % 5 === 0) {
-      value = 'FizzBuzz';
     }
     return value;
   }
