bdeccda35f8f6487aa68b064fbde4a254f016c7f
k2works <kakimomokuri@gmail.com>
2018-09-30 10:36:47 +0900

    開発 最初のテスト作成

--- sam-app/fizz_buzz/tests/unit/test_fizz_buzz.js
+++ sam-app/fizz_buzz/tests/unit/test_fizz_buzz.js
@@ -4,4 +4,7 @@ const FizzBuzz = require('../../src/fizz_buzz');
 const { expect } = chai;
 
 describe('Tests FizzBuzz', () => {
+  it('3ならばFizzを返す', () => {
+    expect(3).to.be.equal('Fizz');
+  });
 });
